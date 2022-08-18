0cfbfe81184e64bec4a87f26fd74b2fbcfec8918
lnorcros <alinachervonec.98@gmail.com>
2022-08-19 00:26:34 +0300

    тестирую секреты

--- fetch.js
+++ fetch.js
@@ -4,8 +4,8 @@ fetch("https://api.tracker.yandex.net/v2/issues/INFRA-47/comments", {
     method: "POST",
     headers: {
         Authorization:
-            "OAuth y0_AgAAAAAu4UNlAAhVwQAAAADMjPMRHBbJVEMcRbiDSieL_EO-CqmLO1w",
-        "X-Org-ID": "7261414",
+            "OAuth ##", // тут будет токен
+        "X-Org-ID": "##", // тут будет id
     },
     body: JSON.stringify({ text: "Тестовый комментарий" }),
 }).then((res) => {
