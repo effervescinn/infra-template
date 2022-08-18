e9e6fb12c77aa2634cd4013f84a7462b66e9b435
lnorcros <alinachervonec.98@gmail.com>
2022-08-19 01:09:14 +0300

    тест отправки запроса на сервер

--- fetch.js
+++ fetch.js
@@ -1,14 +1,13 @@
-// import fetch from "node-fetch";
+import fetch from "node-fetch";
 
-console.log(process.env.id);
-
-// fetch("https://api.tracker.yandex.net/v2/issues/INFRA-47/comments", {
-//     method: "POST",
-//     headers: {
-//         Authorization: `OAuth ${process.env.authToken}`,
-//         "X-Org-ID": `${process.env.id}`,
-//     },
-//     body: JSON.stringify({ text: "Тестовый комментарий" }),
-// }).then(() => {
-//     console.log('3571' + process.env.id);
-// });
+fetch("https://api.tracker.yandex.net/v2/issues/INFRA-47/comments", {
+    method: "POST",
+    headers: {
+        Authorization: `OAuth ${process.env.authToken}`,
+        "X-Org-ID": `${process.env.id}`,
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ text: "Тестовый комментарий" }),
+}).then(() => {
+    console.log("3571" + process.env.id);
+});
