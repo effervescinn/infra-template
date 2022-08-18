608b6864c2bee4ad93481980d75e5c25ef7bf3bc
lnorcros <alinachervonec.98@gmail.com>
2022-08-19 00:56:16 +0300

    опять тест

--- fetch.js
+++ fetch.js
@@ -7,4 +7,6 @@ fetch("https://api.tracker.yandex.net/v2/issues/INFRA-47/comments", {
         "X-Org-ID": `${process.env.id}`,
     },
     body: JSON.stringify({ text: "Тестовый комментарий" }),
+}).then(() => {
+    console.log('3571' + process.env.id);
 });
