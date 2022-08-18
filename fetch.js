da4087a9fa893df6a9fdde0a7dd164ce6364a35f
lnorcros <alinachervonec.98@gmail.com>
2022-08-19 01:02:58 +0300

    опять тест

--- fetch.js
+++ fetch.js
@@ -1,12 +1,14 @@
-import fetch from "node-fetch";
+// import fetch from "node-fetch";
 
-fetch("https://api.tracker.yandex.net/v2/issues/INFRA-47/comments", {
-    method: "POST",
-    headers: {
-        Authorization: `OAuth ${process.env.authToken}`,
-        "X-Org-ID": `${process.env.id}`,
-    },
-    body: JSON.stringify({ text: "Тестовый комментарий" }),
-}).then(() => {
-    console.log('3571' + process.env.id);
-});
+console.log(process.env.id);
+
+// fetch("https://api.tracker.yandex.net/v2/issues/INFRA-47/comments", {
+//     method: "POST",
+//     headers: {
+//         Authorization: `OAuth ${process.env.authToken}`,
+//         "X-Org-ID": `${process.env.id}`,
+//     },
+//     body: JSON.stringify({ text: "Тестовый комментарий" }),
+// }).then(() => {
+//     console.log('3571' + process.env.id);
+// });
