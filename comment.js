7dad5d19430fe0f9ed30e672aae1c0b8b28f0efd
lnorcros <alinachervonec.98@gmail.com>
2022-08-20 14:19:41 +0300

    апдейт скриптов

--- comment.js
+++ comment.js
@@ -1,14 +1,14 @@
 import fetch from "node-fetch";
 
 fetch("https://api.tracker.yandex.net/v2/issues/INFRA-47/comments", {
-    method: "POST",
-    headers: {
-        Authorization: `OAuth ${process.env.authToken}`,
-        "X-Org-ID": `${process.env.id}`,
-        "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ text: `Собрали образ с тегом ${process.env.tag}` }),
+  method: "POST",
+  headers: {
+    Authorization: `OAuth ${process.env.authToken}`,
+    "X-Org-ID": `${process.env.id}`,
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({ text: `Собрали образ с тегом ${process.env.tag}` }),
 }).catch((err) => {
-    console.error("Ошибка при выполнении запроса");
-    throw err;
+  console.error("Ошибка при выполнении запроса");
+  throw err;
 });
--- setRelease.js
+++ setRelease.js
@@ -1,31 +1,31 @@
 import fetch from "node-fetch";
 
 function getDate() {
-    const date = new Date();
-    
-    return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+  const date = new Date();
+
+  return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
 }
 
 const commits = process.argv[2];
 const author = process.env.author;
-const releaseTag = process.env.tag.split("-")[1]
+const releaseTag = process.env.tag.split("-")[1];
 const date = getDate();
 
 fetch("https://api.tracker.yandex.net/v2/issues/INFRA-47", {
-    method: "PATCH",
-    headers: {
-        Authorization: `OAuth ${process.env.authToken}`,
-        "X-Org-ID": `${process.env.id}`,
-        "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-        summary: `Релиз №${releaseTag} от ${date}`,
-        description: `Ответственный за релиз: ${author}
-        
-        Коммиты, попавшие в релиз:
-        ${commits}`,
-    }),
+  method: "PATCH",
+  headers: {
+    Authorization: `OAuth ${process.env.authToken}`,
+    "X-Org-ID": `${process.env.id}`,
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    summary: `Релиз №${releaseTag} от ${date}`,
+    description: `Ответственный за релиз: ${author}
+    
+    Коммиты, попавшие в релиз:
+    ${commits}`,
+  }),
 }).catch((err) => {
-    console.error("Ошибка при выполнении запроса");
-    throw err;
+  console.error("Ошибка при выполнении запроса");
+  throw err;
 });
